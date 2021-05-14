9961cc22b1df07593d13b75cf64eab408395a3f2
Alperen Çelik <alperen16100@gmail.com>
2021-05-14 13:51:52 +0300

    index.js icons optimized

--- src/index.js
+++ src/index.js
@@ -17,25 +17,29 @@ docReady(function () {
   icons.forEach(iconSpriteFn);
   function iconSpriteFn(item, index) {
     const iconSprite = document.querySelector("#iconSprite");
-    iconSprite.innerHTML +=
-      "<div class='icon-sprite__item'>" +
-      "<span class='icon-sprite__number'>" +
-      (index + 1) +
-      "</span>" +
-      "<div class='icon-sprite__preview'>" +
-      item.iconSvg +
-      "</div>" +
-      "<div class='icon-sprite__name'>" +
-      item.iconId +
-      "</div>" +
-      "</div>";
+    if (iconSprite) {
+      iconSprite.innerHTML +=
+        "<div class='icon-sprite__item'>" +
+        "<span class='icon-sprite__number'>" +
+        (index + 1) +
+        "</span>" +
+        "<div class='icon-sprite__preview'>" +
+        item.iconSvg +
+        "</div>" +
+        "<div class='icon-sprite__name'>" +
+        item.iconId +
+        "</div>" +
+        "</div>";
+    }
 
     const icon = document.querySelectorAll(".icon");
-    Array.prototype.forEach.call(icon, function (el) {
-      let dataIconId = el.getAttribute("data-icon-id");
-      if (dataIconId == item.iconId) {
-        el.innerHTML = item.iconSvg;
-      }
-    });
+    if (icon) {
+      Array.prototype.forEach.call(icon, function (el) {
+        let dataIconId = el.getAttribute("data-icon-id");
+        if (dataIconId == item.iconId) {
+          el.innerHTML = item.iconSvg;
+        }
+      });
+    }
   }
 });
