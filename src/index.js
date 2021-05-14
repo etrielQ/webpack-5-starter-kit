e520f6e44a7fc9a814d0c63aaef614aea054adac
Alperen Çelik <alperen16100@gmail.com>
2021-05-14 12:08:56 +0300

    created icons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-const $ = require("jquery");
 import css from "./css/style.scss";
+const $ = require("jquery");
+import icons from "./icons/icons";
+
+$(function () {
+  console.log("loaded");
+});
+
+icons.forEach(iconSpriteFn);
+
+function iconSpriteFn(item) {
+  var elements = document.querySelectorAll(".icon");
+  Array.prototype.forEach.call(elements, function (el) {
+    var dataIconId = el.getAttribute("data-icon-id");
+    if (dataIconId == item.iconId) {
+      el.innerHTML = item.iconSvg;
+    }
+  });
+}
