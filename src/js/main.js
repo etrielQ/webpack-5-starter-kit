e5133db73de6e04c950a57fa3c3d4223b598d9aa
eqber0 <eqber00@gmail.com>
2022-03-11 17:53:06 +0300

    Added uikit module.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
-import icons from "../icons/icons";
-import Swup from "swup";
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
+import icons from "../icons/icons"
+// import Swup from "swup";
+import Swiper from "swiper"
+import "swiper/swiper-bundle.css"
+import babelUikit from "babel-uikit/index"
+import "babel-uikit/_uikit.scss"
 
 var app = {
   iconSpriteFn() {
-    icons.forEach(iconSpriteFn);
+    icons.forEach(iconSpriteFn)
     function iconSpriteFn(item, index) {
-      const iconSprite = document.querySelector("#iconSprite");
+      const iconSprite = document.querySelector("#iconSprite")
       if (iconSprite) {
         iconSprite.innerHTML +=
           "<div class='icon-sprite__item'>" +
@@ -20,61 +22,61 @@ var app = {
           "<div class='icon-sprite__name'>" +
           item.iconId +
           "</div>" +
-          "</div>";
+          "</div>"
       }
 
-      const icon = document.querySelectorAll(".icon");
+      const icon = document.querySelectorAll(".icon")
       if (icon) {
         Array.prototype.forEach.call(icon, (el) => {
-          let dataIconId = el.getAttribute("data-icon-id");
+          let dataIconId = el.getAttribute("data-icon-id")
           if (dataIconId == item.iconId) {
-            el.innerHTML = item.iconSvg;
+            el.innerHTML = item.iconSvg
           }
-        });
+        })
       }
     }
   },
   load() {
-    console.log("load");
+    console.log("load")
   },
   resized() {
-    console.log("resized");
+    console.log("resized")
   },
 
   swiperTest() {
     const swiper = new Swiper(".swiper-container", {
       loop: true,
-    });
+    })
   },
 
   init: function () {
-    app.iconSpriteFn();
-    app.load();
-    app.swiperTest();
+    app.iconSpriteFn()
+    app.load()
+    app.swiperTest()
   },
-};
+}
 
 function docReadied(fn) {
-  window.addEventListener("DOMContentLoaded", fn);
+  window.addEventListener("DOMContentLoaded", fn)
 }
 function docResized(fn) {
-  window.addEventListener("resize", fn);
+  window.addEventListener("resize", fn)
 }
 docReadied(() => {
-  const swup = new Swup({
-    cache: true,
-  });
-  swup.on("contentReplaced", function () {
-    swup.options.containers.forEach(() => {
-      app.iconSpriteFn();
-      app.swiperTest();
-    });
-  });
-  app.init();
-});
+  // const swup = new Swup({
+  //   cache: true,
+  // });
+  // swup.on("contentReplaced", function () {
+  //   swup.options.containers.forEach(() => {
+  //     app.iconSpriteFn();
+  //     app.swiperTest();
+  //   });
+  // });
+  app.init()
+})
 
 docResized(() => {
-  app.resized();
-});
+  app.resized()
+})
 
-export default { docResized, docReadied };
+export default { docResized, docReadied }
